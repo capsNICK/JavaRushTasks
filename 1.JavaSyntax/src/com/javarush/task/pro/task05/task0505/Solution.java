ab555f33b017254d74075bd5ecfc9f11744e2a3c
capsNICK <alexandervmalysh@gamil.com>
2022-10-10 22:46:40 +0500

    Task task0505 has been updated.

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -11,12 +11,19 @@ public class Solution {
         //напишите тут ваш код
         Scanner console = new Scanner(System.in);
         int n = console.nextInt();
-        
         int[] array = new int[n];
         
         for (int i = 0; i < n; i++) {
-            array[i] = console.nextInt(); //test_1
+            array[i] = console.nextInt();
         }
-        
+        if (n % 2 == 0) {
+            for (int i = n - 1; i >= 0; i--) {
+                System.out.println(array[i]);
+            }
+        } else {
+            for (int i = 0; i < n; i++) {
+                System.out.println(array[i]);
+            }
+        }
     }
 }
